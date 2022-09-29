aef0d6f2adb5cd2923aafc7d11fc3e813f5d52eb
Lisseth Martinez <lis.seth2k16@gmail.com>
2022-09-28 20:42:00 -0500

    first commit

---
+++ database/migrations/2022_09_29_011402_create_productos_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('productos', function (Blueprint $table) {
+            $table->id('id');
+            $table->char('telefono',15);
+            $table->smallInteger('stock');
+            $table->string('detalle');
+            $table->string('tipo');
+            $table->char('talla',2);
+           // $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('productos');
+    }
+};
